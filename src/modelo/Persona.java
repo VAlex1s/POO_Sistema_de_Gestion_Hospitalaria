73cdc2a26f49587c14f934da464e418ebd0c8078
victor <victor@keys>
2026-06-04 20:44:13 -0500

    creacion clase persona

---
+++ src/modelo/Persona.java
@@ -0,0 +1,78 @@
+package modelo;
+
+public abstract class Persona {
+
+    private String dni;
+    private String nombres;
+    private String apellidos;
+    private String telefono;
+    private String correo;
+    private String direccion;
+
+    public Persona() {
+    }
+
+    public Persona(String dni, String nombres, String apellidos,
+            String telefono, String correo, String direccion) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombreCompleto() {
+        return nombres + " " + apellidos;
+    }
+
+    public abstract String mostrarInformacion();
+}
